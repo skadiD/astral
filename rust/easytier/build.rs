f74cfe57ae65a58670779f6f4c598c2b8da7762a
会做饭的二哈 <baikaiwen12@outlook.com>
2025-07-03 12:31:07 +0800

    2.1.11

--- rust/easytier/build.rs
+++ rust/easytier/build.rs
@@ -128,13 +128,13 @@ fn check_locale() {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // enable thunk-rs when target os is windows and arch is x86_64 or i686
-    #[cfg(target_os = "windows")]
-    if !std::env::var("TARGET")
-        .unwrap_or_default()
-        .contains("aarch64")
-    {
-        thunk::thunk();
-    }
+    // #[cfg(target_os = "windows")]
+    // if !std::env::var("TARGET")
+    //     .unwrap_or_default()
+    //     .contains("aarch64")
+    // {
+    //     thunk::thunk();
+    // }
 
     #[cfg(target_os = "windows")]
     WindowsBuild::check_for_win();
